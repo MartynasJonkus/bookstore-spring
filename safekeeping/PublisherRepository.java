a90581cc418a4dceb2065559981d60615d86d9bd
MartynasJonkus <martynez00@gmail.com>
2025-03-18 11:21:54 +0200

    Removed safekeeping

--- safekeeping/PublisherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.repository;
-
-import com.bookstore.model.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PublisherRepository extends JpaRepository<Publisher, Long> {
-}
